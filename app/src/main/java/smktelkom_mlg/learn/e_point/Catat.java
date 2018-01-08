c4dff6a59bffa149ca1d7450f5acdc42ca4ffce3
sadilasadil <salsaoctavia2@gmail.com>
2018-01-08 10:24:12 +0700

    add spinner

--- app/src/main/java/smktelkom_mlg/learn/e_point/Catat.java
+++ app/src/main/java/smktelkom_mlg/learn/e_point/Catat.java
@@ -1,13 +1,150 @@
 package smktelkom_mlg.learn.e_point;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Catat extends AppCompatActivity {
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@SuppressLint("NewApi")
+public class Catat extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catat);
+        // Permission StrictMode
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        // spinner1
+        final Spinner spin = (Spinner) findViewById(R.id.spinner1);
+
+        String url = "http://10.0.2.2/spinner3/getData.php";
+
+        try {
+
+            JSONArray data = new JSONArray(getJSONUrl(url));
+
+            final ArrayList<HashMap<String, String>> MyArrList = new ArrayList<HashMap<String, String>>();
+            HashMap<String, String> map;
+
+            for (int i = 0; i < data.length(); i++) {
+                JSONObject c = data.getJSONObject(i);
+
+                map = new HashMap<String, String>();
+                //map.put("id_siswa", c.getString("id_siswa"));
+                //map.put("nis", c.getString("nis"));
+                map.put("nama", c.getString("nama"));
+                MyArrList.add(map);
+
+            }
+            SimpleAdapter sAdap;
+            sAdap = new SimpleAdapter(Catat.this, MyArrList, R.layout.activity_show,
+                    new String[]{/*"id_siswa", "nis",*/ "nama"}, new int[]{/*R.id.ColID, R.id.ColNis,*/ R.id.ColNama});
+            spin.setAdapter(sAdap);
+
+            final AlertDialog.Builder viewDetail = new AlertDialog.Builder(this);
+
+            spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                public void onItemSelected(AdapterView<?> arg0, View selectedItemView,
+                                           int position, long id) {
+                    //String id_siswa = MyArrList.get(position).get("id_siswa").toString();
+                    //String nis = MyArrList.get(position).get("nis").toString();
+                    String nama = MyArrList.get(position).get("nama")
+                            .toString();
+
+                    /*viewDetail.setIcon(android.R.drawable.btn_star_big_on);
+                    viewDetail.setTitle("Siswa");
+                    viewDetail.setMessage("Id Siswa : " + id_siswa + "\n"
+                            + "Nis : " + nis + "\n" + "Nama : " + nama);
+                    viewDetail.setPositiveButton("OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int which) {
+                                    // TODO Auto-generated method stub
+                                    dialog.dismiss();
+                                }
+                            });
+                    viewDetail.show();*/
+
+                }
+
+                public void onNothingSelected(AdapterView<?> arg0) {
+                    // TODO Auto-generated method stub
+                    Toast.makeText(Catat.this,
+                            "Your Selected : Nothing",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
+
+    public String getJSONUrl(String url) {
+        StringBuilder str = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) { // Download OK
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    str.append(line);
+                }
+            } else {
+                Log.e("Log", "Failed to download result..");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return str.toString();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
 }
